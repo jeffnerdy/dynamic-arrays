6adcd36ed0161b5da61765876f68870ac0ddfc64
jeffnerdy <110793443+jeffnerdy@users.noreply.github.com>
2024-04-22 16:38:14 +0800

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,6 +1,7 @@
 #pragma once
 
-//#include <iostream>
+#include <iostream>
+//#include <memory>
 #include <cstring>
 
 
@@ -8,29 +9,26 @@ template<typename T>
 class Vector
 {
 private:
-	T* m_ptr;
-	T* m_tempptr;
-	size_t m_size;
-	size_t m_tempsize;
-	size_t m_reserved;
-	size_t m_index;
+	T* m_ptr = nullptr;
+	size_t m_size = 0;
+	//size_t m_capacity = 0;
 
 public:
-	void push_back(T data);
+	void push_back(T& data);
+	void push_back(T&& data);
 
 	void resize(size_t size);
-	void reserve(size_t size);
+	//void reserve(size_t size);
+
 	void erase(size_t dst);
-	void erase(size_t dst, size_t src);
+	void erase(size_t src, size_t dst);
 
 	void clear();
 
 	size_t size() const;
-	size_t capacity() const;
+	//size_t capacity() const;
 
 	T& operator[](size_t index) const;
-
-	void reallocate(size_t reserved, size_t size);
 
 public:
 	Vector();
@@ -41,66 +39,51 @@ public:
 };
 
 template<typename T>
-void Vector<T>::push_back(T data)
+void Vector<T>::push_back(T& data)
 {
-	if (!(m_index < m_reserved))
+	if (!(m_size < 0))
 	{
-		this->reallocate(m_size + 1, m_size);
-
-		if (m_reserved < m_tempsize) m_reserved = m_tempsize;
-		m_tempsize = 0;
+		resize(m_size + 1);
 	}
 
-	m_ptr[m_index] = data;
-	m_index++;
+	new (m_ptr + m_size) T(data);
+	m_size++;
+}
 
-	if (!(m_index < m_size))
+template<typename T>
+void Vector<T>::push_back(T&& data)
+{
+	if (!(m_size < 0))
 	{
-		m_size = m_index;
+		resize(m_size + 1);
 	}
+
+	new (m_ptr + m_size) T(data);
+	m_size++;
 }
 
 template<typename T>
 void Vector<T>::resize(size_t size)
 {
-	if (m_size != size)
+	if (m_size != size && size != 0)
 	{
-		if (size < m_size)
+		if (m_size == 0)
 		{
-			this->reallocate(size, size);
-			m_reserved = size;
-			m_index = size;
+			m_ptr = new T[size]();
+
+			m_size = size;
 		}
-		else {
-			this->reallocate(size, m_size);
+		else
+		{
+			T* temp_ptr = new T[size]();
+
+			std::uninitialized_copy(m_ptr, m_ptr + size, temp_ptr);
+
+			delete[] m_ptr;
+			m_ptr = temp_ptr;
+			m_size = size;
 		}
-
-		m_size = m_tempsize;
-		m_tempsize = 0;
 	}
-}
-
-template<typename T>
-void Vector<T>::reserve(size_t size)
-{
-	if (size > m_reserved)
-	{
-		this->reallocate(size, m_size);
-
-		m_reserved = m_tempsize;
-		m_tempsize = 0;
-	}
-}
-
-template<typename T>
-void Vector<T>::clear()
-{
-	m_tempptr = new T[m_size];
-	delete[] m_ptr;
-	m_ptr = m_tempptr;
-	m_tempptr = nullptr;
-
-	m_index = 0;
 }
 
 template<typename T>
@@ -110,89 +93,95 @@ void Vector<T>::erase(size_t dst)
 	{
 		if ((m_size - dst) == 1)
 		{
+			m_ptr[dst].~T();
 			m_size--;
-			m_index--;
 		}
 		else {
-			std::memcpy(m_ptr + dst, m_ptr + (dst + 1), (m_size - (dst + 1)) * sizeof(T));
+			m_ptr[dst].~T();
+
+			std::uninitialized_copy(m_ptr + (dst + 1), (m_ptr + (dst + 1)) + (m_size - (dst + 1)), m_ptr + dst);
 			m_size--;
-			m_index--;
 		}
 	}
 }
 
 template<typename T>
-void Vector<T>::erase(size_t dst, size_t src)
+void Vector<T>::erase(size_t src, size_t dst)
 {
-	if (dst < src && src < m_size)
+	if (src <= dst && dst < m_size)
 	{
-		if ((m_size - dst) == 1)
+		if ((m_size - src) == 1)
 		{
+			for (int i = dst; i >= src; i--)
+			{
+				m_ptr[i].~T();
+			}
 			m_size--;
-			m_index--;
 		}
 		else {
-			std::memcpy(m_ptr + dst, m_ptr + (src + 1), (m_size - (src + 1)) * sizeof(T));
-			m_size -= (src - dst) + 1;
-			m_index = m_size;
+			for (int i = dst; i >= src; i--)
+			{
+				m_ptr[i].~T();
+			}
+
+			if(!((m_size - dst) == 1)) std::uninitialized_copy(m_ptr + (dst + 1), (m_ptr + (dst + 1)) + (m_size - (dst + 1)), m_ptr + src);
+
+			m_size -= (dst - src) + 1;
 		}
 	}
+}
+
+template<typename T>
+void Vector<T>::clear()
+{
+	for (int i = m_size - 1; i >= 0; i--)
+	{
+		m_ptr[i].~T();
+	}
+
+	m_size = 0;
 }
 
 template<typename T>
 size_t Vector<T>::size() const
 {
-	return this->m_size;
-}
-
-template<typename T>
-size_t Vector<T>::capacity() const
-{
-	return this->m_reserved;
+	return m_size;
 }
 
 template<typename T>
 T& Vector<T>::operator[](size_t index) const
 {
-	if (index < m_index) return *(m_ptr + index);
+	if (index < m_size) {
+		return *(m_ptr + index);
+	}
+	else {
+		throw std::out_of_range("error: index out of range");
+	}
 }
 
 template<typename T>
-void Vector<T>::reallocate(size_t reserved, size_t size)
-{
-	m_tempsize = reserved;
-	m_tempptr = new T[m_tempsize];
-	std::memcpy(m_tempptr, m_ptr, size * sizeof(T));
-	delete[] m_ptr;
-	m_ptr = m_tempptr;
-	m_tempptr = nullptr;
-}
-
-template<typename T>
-Vector<T>::Vector() : m_ptr(nullptr), m_tempptr(nullptr), m_size(0), m_tempsize(0), m_reserved(0), m_index(0)
+Vector<T>::Vector()
 {}
 
 template<typename T>
-Vector<T>::Vector(std::initializer_list<T> list) : m_tempptr(nullptr), m_size(0), m_tempsize(0), m_reserved(0), m_index(0)
+Vector<T>::Vector(std::initializer_list<T> list)
 {
-	m_ptr = new T[list.size()];
+	resize(list.size());
+
 	for (auto element : list)
 	{
 		this->push_back(element);
 	}
-	//std::cout << "Constructed!" << std::endl;
 }
 
 template<typename T>
-Vector<T>::Vector(size_t size) : m_tempptr(nullptr), m_size(size), m_tempsize(0), m_reserved(size), m_index(0)
+Vector<T>::Vector(size_t size)
 {
-	m_ptr = new T[m_size];
-	//std::cout << "Constructed!" << std::endl;
+	resize(size);
 }
 
 template<typename T>
 Vector<T>::~Vector()
 {
-	delete[] m_ptr;
-	//std::cout << "Destructed!" << std::endl;
+	if(m_size != 0) delete[] m_ptr;
 }
